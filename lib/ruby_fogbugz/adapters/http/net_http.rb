e00e9785c4d2ab53834b00fb1607f8b3c358f4e3
keiths-osc <keiths-osc@users.noreply.github.com>
2017-02-09 21:45:02 -0500

    Added an option to specify a CA cert bundle for https servers.  Use double quotes around \n in gemspec so it's treated as a newline character (#17)

--- lib/ruby_fogbugz/adapters/http/net_http.rb
+++ lib/ruby_fogbugz/adapters/http/net_http.rb
@@ -10,6 +10,7 @@ module Fogbugz
 
         def initialize(options = {})
           @root_url = options[:uri]
+          @ca_file = options[:ca_file]
         end
 
         def build_request(uri, params)
@@ -29,7 +30,10 @@ module Fogbugz
           request = build_request(uri, params)
 
           http = Net::HTTP.new(uri.host, uri.port)
-          http.use_ssl = @root_url.start_with? 'https'
+          if @root_url.start_with? 'https'
+            http.use_ssl = true
+            http.ca_file = @ca_file
+          end
 
           response = http.start { |h| h.request(request) }
           response.body
